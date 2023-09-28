16579b217eab2e55d0bc67927c3597cd765a2056
Luizo <emanuele109@hotmail.it>
2023-09-28 16:44:57 +0200

    squeal put test :>

--- login.js
+++ login.js
@@ -11,3 +11,34 @@ function sub(){
   });
 }
 
+function addSquealTest(){
+  fetch("https://site222326.tw.cs.unibo.it/squeals", {
+    method: "PUT",
+    body: JSON.stringify({
+        id: "test",
+        author: "Gigi",
+        text: "I'm.",
+        receiver: "Lana",
+        date: 745675,
+        positive_reactions: 2,
+        positive_reactions_users: [
+          "Luiso",
+          "Matilde"
+        ],
+        negative_reactions: 1,
+        negative_reactions_users: [
+          "Alex",
+        ],
+        media: "URI...",
+        reply_to: ["squeal1"],
+        replies_num: 0,
+        replies: ["id1", "id2"],
+        keywords: ["sea", "mountain"],
+        mentions: ["user1"],
+        impressions: 34
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    });
+  }
--- squeal.js
+++ squeal.js
@@ -4,11 +4,16 @@ const { app, mongoClient } = require("./index.js");
 const dbName = "SquealerDB";
 const squealCollection = "Squeals";
 
+// ritorna una lista di squeal del database, da startindex ad endindex
+// bisogna specificare l'autore degli squeal
 app.get("/squeals/", async (req, res) => {
     try {
+        // take the parameters from the request
         let author = req.query.author;
         let startIndex = parseInt(req.query.startindex);
         let endIndex = parseInt(req.query.endindex);
+
+        // check if the parameters are valid
         if (author === undefined || startIndex === undefined || startIndex === NaN || endIndex === undefined || endIndex === NaN) {
             res.status(400).json({ message: "author, startindex and endindex are required" });
             return;
@@ -21,7 +26,8 @@ app.get("/squeals/", async (req, res) => {
             .sort({ timestamp: -1 }) // ordered inverse chronological order
             .skip(startIndex) // starting from startIndex
             .limit(endIndex); // returns endIndex squeals
-        res.status(200).json(squeals);
+        res.status(200).json(squeals); // returns the squeals
+
     } catch (error) {
         res.status(500).json({ message: error.message });
     } finally {
@@ -31,12 +37,44 @@ app.get("/squeals/", async (req, res) => {
 
 // aggiungere uno squeal al database
 //todo controllare il contenuto del req.body per verificare se il contenuto è corretto
+//todo controllare se l'utente è loggato
+/* required fields for req.body:
+"id", "author", "text", "receiver", "date", "positive_reactions", "positive_reactions_users", "negative_reactions", "negative_reactions_users", "media", "reply_to", "replies_num", "replies", "keywords", "mentions", "impressions"
+*/
 app.put("/squeals", bodyParser.json(), async (req, res) => {
     try {
+        const requiredFields = [
+            "id",
+            "author",
+            "text",
+            "receiver",
+            "date",
+            "positive_reactions",
+            "positive_reactions_users",
+            "negative_reactions",
+            "negative_reactions_users",
+            "media",
+            "reply_to",
+            "replies_num",
+            "replies",
+            "keywords",
+            "mentions",
+            "impressions"
+        ];
+
+        // Check if all required fields are present in the request body
+        for (const field of requiredFields) {
+            if (req.body[field] === undefined) {
+                res.status(400).json({ message: `${field} is required` });
+                return;
+            }
+        }
+        // If all required fields are present, continue with the insertion
+
         await mongoClient.connect();
         const database = mongoClient.db();
 
-        const result = await collezione.insertOne(res.body);
+        const result = await collezione.insertOne(req.body);
 
         console.log('Documento inserito con successo:', result.insertedId);
     } catch (errore) {
