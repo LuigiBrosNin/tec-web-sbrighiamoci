f52d5bfb770a30e863eca43cc19940a3de350939
Luizo <emanuele109@hotmail.it>
2023-09-29 17:34:22 +0200

    Update squeal.js

--- squeal.js
+++ squeal.js
@@ -36,11 +36,10 @@ app.get("/squeals/", async (req, res) => {
         const database = mongoClient.db(dbName);
         const collection = database.collection(squealCollection);
         const squeals = await collection.find({ author }) // returns the squeals made by author
-            //.sort({ timestamp: -1 }); // ordered inverse chronological order
-            //.skip(startIndex) // starting from startIndex
-            //.limit(endIndex); // returns endIndex squeals
+            .sort({ timestamp: -1 }) // ordered inverse chronological order
+            .skip(startIndex) // starting from startIndex
+            .limit(endIndex); // returns endIndex squeals
 
-        console.log('Squeals found:', squeals.length);
         console.log('Squeals:', JSON.stringify(squeals));
         
         res.status(200).json(squeals); // returns the squeals
