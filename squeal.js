2dbfb26053a372dc141dc762fe639c9868484842
Luizo <emanuele109@hotmail.it>
2023-10-01 14:43:09 +0200

    my functions are tested, yay

--- squeal.js
+++ squeal.js
@@ -592,8 +592,6 @@ app.get("/squeals/:id/replies/", async (req, res) => {
         // only getting the IDs of the wanted replies
         let idsOfSquealRepliesToReturn = main_squeal.replies.slice(startIndex, endIndex);
 
-        console.log('IDs of Replies:', JSON.stringify(idsOfSquealRepliesToReturn));
-
         // what we'll return
         let squealReplies = [];
 
