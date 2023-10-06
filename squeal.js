c7814fdc7624ee0b8e302c8633975dd783221747
Luizo <emanuele109@hotmail.it>
2023-10-06 17:18:26 +0200

    Update squeal.js

--- squeal.js
+++ squeal.js
@@ -142,7 +142,7 @@ app.get("/squeals/", async (req, res) => {
         const database = mongoClient.db(dbName);
         const collection = database.collection(squealCollection);
 
-        const squeals = await collection.find({id: "EmanueleDiSante1"});
+        const squeals = await collection.find({id: "EmanueleDiSante1"}).toArray();
 /*
         const squeals = await collection.find(search)
             .sort({
