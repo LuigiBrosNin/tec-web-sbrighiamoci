6905516e832966058279779c3a3eb7ae7f6290c0
mettil13 <matildemariano01@gmail.com>
2023-10-19 17:26:32 +0200

    i'm so f***ing stupid

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.get("/logout", async (req, res) => {
 
 app.put("/signin", async (req, res) => {
   if(req.body.username != null && req.body.username !== "" && req.body.email != null && req.body.email !== "" && req.body.password != null && req.body.password !== ""){ // the check var == null is equivalent to var === null && var === undefined
-    req.session.user = Arturo;
+    req.session.user = "Arturo";
     let status = await registerNewUser(req.body.username, req.body.email,req.body.password);
     req.session.user = undefined;
     res.status(status).send();
