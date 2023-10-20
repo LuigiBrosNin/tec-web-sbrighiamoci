8ce5f5e0c2b58130e93ec4e732aaf25dd3470d7f
Luizo <emanuele109@hotmail.it>
2023-10-20 19:14:50 +0200

    added /channels get query

--- channels.js
+++ channels.js
@@ -30,10 +30,75 @@ const collection_channels = database.collection(channelCollection);
 
 /********************** FUNCTIONS **********************/
 
+/* -------------------------------------------------------------------------- */
+/*                                 /CHANNELS/                                 */
+/*                                    GET                                     */
+/* -------------------------------------------------------------------------- */
+
+//* GET
+// returns the list of the channels
+// supports pagination & queries
+// parameters: startindex, endindex, name, owner, type (privileged, private)
+// GTE prameters: subscribers_num
+
+//TODO TEST THE FUNCTION
+app.get("/channels", async (req, res) => {
+  try {
+
+    // initializing the start and end index in case they are not specified
+    let startIndex = 0;
+    let endIndex = 10;
+    // check if the parameters exist
+    if (req.query.startindex !== undefined && req.query.startindex !== NaN) {
+      startIndex = parseInt(req.query.startindex);
+    }
+    if (req.query.endindex !== undefined && req.query.endindex !== NaN) {
+      endIndex = parseInt(req.query.endindex);
+    }
+    // check if indexes are valid
+    if (startIndex > endIndex) {
+      res.status(400).json({
+        message: "startIndex must be less than endIndex"
+      });
+      return;
+    }
+
+    let search = {};
+    const possibleParams = ["name", "owner", "type"];
+
+    for (const param of possibleParams) {
+      if (req.query[param] !== undefined) {
+        search[param] = req.query[param];
+      }
+    }
+
+    // check number of subscribers request
+    if (req.query.subscribers_num !== undefined && req.query.subscribers_num !== NaN) {
+      search.subscribers_num = {
+        $gte: parseInt(req.query.subscribers_num)
+      };
+    }
+
+    await mongoClient.connect();
+    const channels = await collection_channels.find(search)
+      .sort({
+        timestamp: -1
+      }) // ordered inverse chronological order
+      .skip(startIndex) // starting from startIndex
+      .limit(endIndex) // returns endIndex squeals
+      .toArray(); // returns the squeals as an array
+
+    res.status(200).json(channels);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
 
 /* -------------------------------------------------------------------------- */
-/*                   /CHANNELS/:NAME/SUBSCRIBERS_NUM                          */
-/*                                GET                                         */
+/*                       /CHANNELS/:NAME/SUBSCRIBERS_NUM                      */
+/*                                   GET                                      */
 /* -------------------------------------------------------------------------- */
 
 //* GET
@@ -43,19 +108,19 @@ app.get("/channels/:name/subscribers_num", async (req, res) => {
     const channelName = req.params.name
 
     await mongoClient.connect()
-    const channel = await collection_channels.findOne({ name: channelName })
+    const channel = await collection_channels.findOne({
+      name: channelName
+    })
 
     if (!channel) {
       res.status(404).json({
         message: "Channel not found."
       });
       return;
-    }
-    else {
+    } else {
       res.status(200).json(channel.subscribers_num)
     }
-  }
-  catch (error) {
+  } catch (error) {
     res.status(500).json({
       message: error.message
     });
@@ -73,7 +138,7 @@ app.get("/channels/:name/subscribers_num", async (req, res) => {
 
 app.get("/channels/:name/subscribers_list", async (req, res) => {
   try {
-  console.log("Benvenuto in quasta nuva get")
+    console.log("Benvenuto in quasta nuva get")
     // initializing the start and end index in case they are not specified
     let startIndex = 0;
     let endIndex = 10;
@@ -96,7 +161,9 @@ app.get("/channels/:name/subscribers_list", async (req, res) => {
 
     await mongoClient.connect();
 
-    const channel = await collection_channels.findOne({ name: channelName })
+    const channel = await collection_channels.findOne({
+      name: channelName
+    })
     // if the channel is not found, return 404
     if (channel === null) {
       res.status(404).json({
@@ -107,9 +174,10 @@ app.get("/channels/:name/subscribers_list", async (req, res) => {
 
     const subscribers = channel.subscribers_list.slice(startIndex, endIndex + 1);
     res.status(200).json(subscribers);
-  }
-  catch (error) {
-    res.status(500).json({ message: error.message });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
   }
 });
 
@@ -136,9 +204,10 @@ app.put("/channels/:name/subscribers_list", async (req, res) => {
       iscriverlo (aggiungerlo a subscribers_list e andare nel suo profilo ad aggiungere il follow)
   */
   try {
-    
-  }
-  catch (error) {
-    res.status(500).json({ message: error.message });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
   }
 });
