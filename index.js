9a4bd5be179e8ce5dea79d6b255afd295377b34d
mettil13 <matildemariano01@gmail.com>
2023-10-20 12:48:30 +0200

    debug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ global.rootDir = __dirname;
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+//const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const app = express();
 
@@ -73,7 +73,7 @@ app.get("/logout", async (req, res) => {
   res.redirect("/");
 })
 
-app.put("/signin", cookieParser.json(), async (req, res) => {
+app.put("/signin", /*cookieParser.json(),*/ async (req, res) => {
   if(req.body.username != null && req.body.username !== "" && req.body.email != null && req.body.email !== "" && req.body.password != null && req.body.password !== ""){ // the check var == null is equivalent to var === null && var === undefined
     req.session.user = "Arturo";
     console.log(req.cookies);
