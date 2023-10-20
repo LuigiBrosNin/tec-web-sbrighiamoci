7612b662d2f634694c67fc9eed70933f8e783ee5
alexlorenzato <alexlorenzato@gmail.com>
2023-10-20 16:56:40 +0200

    testGetSubs

--- channels.js
+++ channels.js
@@ -17,7 +17,7 @@ const {
   dbName,
   squealCollection,
   profileCollection,
-  channelCollection, 
+  channelCollection,
   mongoClient,
   CM
 } = require("./const.js");
@@ -30,12 +30,18 @@ const collection_channels = database.collection(channelCollection);
 
 /********************** FUNCTIONS **********************/
 
+
+/* -------------------------------------------------------------------------- */
+/*                           /CHANNELS/:NAME                                  */
+/*                                GET                                         */
+/* -------------------------------------------------------------------------- */
+
 //* GET
 // return the number of subscribers of a channel
 app.get("/channels/:name", async (req, res) => {
   try {
     const channelName = req.params.name
-    
+
     await mongoClient.connect()
     const channel = await collection_channels.findOne({ name: channelName })
 
@@ -43,7 +49,7 @@ app.get("/channels/:name", async (req, res) => {
       res.status(404).json({
         message: "Channel not found."
       });
-      return; 
+      return;
     }
     else {
       res.status(200).json(channel.subscribers_num)
@@ -53,5 +59,56 @@ app.get("/channels/:name", async (req, res) => {
     res.status(500).json({
       message: error.message
     });
+  }
+});
+
+
+/* -------------------------------------------------------------------------- */
+/*                            /CHANNELS/FOLLOWERS                             */
+/*                              GET, PUT, DELETE                              */
+/* -------------------------------------------------------------------------- */
+
+//* GET
+// returns the list of the subscribers of the channel
+
+app.get("/channels/:name", async (req, res) => {
+  try {
+  console.log("Benvenuto in quasta nuva get")
+    // initializing the start and end index in case they are not specified
+    let startIndex = 0;
+    let endIndex = 10;
+
+    // check if the parameters are valid
+    if (req.query.startindex !== undefined && req.query.startindex !== NaN) {
+      startIndex = parseInt(req.query.startindex);
+    }
+    if (req.query.endindex !== undefined && req.query.endindex !== NaN) {
+      endIndex = parseInt(req.query.endindex);
+    }
+    if (startIndex > endIndex) {
+      res.status(400).json({
+        message: "startIndex must be less than endIndex."
+      });
+      return;
+    }
+
+    const channelName = req.params.name;
+
+    await mongoClient.connect();
+
+    const channel = await collection_channels.findOne({ name: channelName })
+    // if the channel is not found, return 404
+    if (channel === null) {
+      res.status(404).json({
+        message: "channel not found"
+      });
+      return;
+    }
+
+    const subscribers = channel.subscribers_list.slice(startIndex, endIndex + 1);
+    res.status(200).json(subscribers);
+  }
+  catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
