9159f58ea2f812231d7e942079bc121ed0a136c3
mettil13 <matildemariano01@gmail.com>
2023-10-20 12:43:32 +0200

    debug

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.get("/logout", async (req, res) => {
   res.redirect("/");
 })
 
-app.put("/signin", async (req, res) => {
+app.put("/signin", cookieParser.json(), async (req, res) => {
   if(req.body.username != null && req.body.username !== "" && req.body.email != null && req.body.email !== "" && req.body.password != null && req.body.password !== ""){ // the check var == null is equivalent to var === null && var === undefined
     req.session.user = "Arturo";
     console.log(req.cookies);
