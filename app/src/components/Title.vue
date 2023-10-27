40bc84cd8962d976bd72e0474f1b0fb79a4ba2df
mettil13 <matildemariano01@gmail.com>
2023-10-27 14:07:59 +0200

    app structure

---
+++ app/src/components/Title.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="title">
+      <h2>Squealer</h2>
+    </div>
+</template>
+  
+<style scoped>
+  .title {
+    font-size: 2em;
+  }
+</style>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SearchView from '../views/SearchView.vue'
+import ChannelsView from '../views/ChannelsView.vue'
+import MessagesView from '../views/MessagesView.vue'
+import ProfileView from '../views/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +14,24 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/search',
+      name: 'search',
+      component: SearchView
+    },
+    {
+      path: '/channels',
+      name: 'channels',
+      component: ChannelsView
+    },
+    {
+      path: '/messages',
+      name: 'messages',
+      component: MessagesView
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView
     }
   ]
 })
