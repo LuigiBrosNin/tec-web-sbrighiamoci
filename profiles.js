df5c0bfb4a6f25e14d34372f835b060c6c5ad977
alexlorenzato <alexlorenzato@gmail.com>
2023-10-27 16:04:01 +0200

    fix

--- profiles.js
+++ profiles.js
@@ -287,7 +287,7 @@ app.delete("/profiles/:name", async (req, res) => {
 console.log("Sei nella profiles delete e channels_owned: " + JSON.stringify(channels_owned))
             for (const channel of channels_owned) { 
                 console.log("channel.mods_list[0]: " + channel.mods_list[0])
-                if (channel.mods_list[0] !== "" || channel.mods_list[0] !== undefined) { // there is a mod //! per niente sicuro di questo controllo
+                if (channel.mods_list[0] !== "" && channel.mods_list[0] !== undefined) { // there is a mod //! per niente sicuro di questo controllo
                     const new_mod = channel.mods_list[0];
 
                     await mongoClient.connect();
