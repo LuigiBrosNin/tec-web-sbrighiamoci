f1e6eb6e940ff965010c7956409917d3a3c1a258
mettil13 <matildemariano01@gmail.com>
2023-10-29 02:05:05 +0200

    better CORS

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.set('trust proxy', 1); // trust first proxy
 let cors = require('cors');
 app.use(cors({
   //origin: BASE_SITE,
-  //credentials: true,
+  credentials: true,
 }));
 
 // used for login
