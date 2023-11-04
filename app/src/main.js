90b563ce24398e165318148cf3d3804967bba5a2
mettil13 <matildemariano01@gmail.com>
2023-11-04 01:35:00 +0100

    build fix

--- app/src/main.js
+++ app/src/main.js
@@ -24,11 +24,15 @@ const app = createApp(App);
 app.use(router);
 
 
-
+// vue does not support await at top level
+/*
 let user = await fetch("https://site222326.tw.cs.unibo.it/user-check", { method: "GET"});
 user = await user.json();
 app.config.globalProperties.$user = user.user;
 
-
-
 app.mount('#app');
+*/
+fetch("https://site222326.tw.cs.unibo.it/user-check", { method: "GET"})
+    .then(res => res.json())
+    .then(res => app.config.globalProperties.$user = res.user)
+    .then(app.mount('#app')); // this have to be always the last action
