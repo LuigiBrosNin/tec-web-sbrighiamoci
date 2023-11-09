bfba001d3a57ee6aae8f99743a22fc01e1105610
Luizo <emanuele109@hotmail.it>
2023-11-09 18:10:13 +0100

    better format

--- app/src/views/SearchView.vue
+++ app/src/views/SearchView.vue
@@ -245,7 +245,9 @@ export default {
         keyword: "",
         mentions: "",
         isPrivate: false,
-      },
+        fetchURI: `https://site222326.tw.cs.unibo.it/squeals/?startindex=${this.startIndex}&endindex=${this.endIndex}&author=${this.query.author}&popularity=${this.query.popularity}&start_date=${this.query.startDate}&end_date=${this.query.endDate}&positive_reactions=${this.query.positiveReactions}&negative_reactions=${this.query.negativeReactions}&impressions=${this.query.impressions}&receiver=${this.query.receiver}&keyword=${this.query.keyword}&mentions=${this.query.mentions}&is_private=${this.query.isPrivate}`,
+        fetchNextURI: `https://site222326.tw.cs.unibo.it/squeals/?startindex=${this.startIndex+11}&endindex=${this.endIndex+11}&author=${this.query.author}&popularity=${this.query.popularity}&start_date=${this.query.startDate}&end_date=${this.query.endDate}&positive_reactions=${this.query.positiveReactions}&negative_reactions=${this.query.negativeReactions}&impressions=${this.query.impressions}&receiver=${this.query.receiver}&keyword=${this.query.keyword}&mentions=${this.query.mentions}&is_private=${this.query.isPrivate}`,
+    },
     };
   },
   computed: {
@@ -267,9 +269,7 @@ export default {
     } else {
       // check if next page is empty for sure
       const response2 = await fetch(
-        `https://site222326.tw.cs.unibo.it/squeals/?startindex=${
-          this.startIndex + 11
-        }&endindex=${this.endIndex + 11}`
+        `https://site222326.tw.cs.unibo.it/squeals/?startindex=${this.startIndex + 11}&endindex=${this.endIndex + 11}`
       );
       const feed2 = await response2.json();
       console.log("feed2 length: " + feed2.length);
@@ -287,23 +287,7 @@ export default {
       this.currentPage = 1;
       this.startIndex = 0;
       this.endIndex = 9;
-      const response = await fetch(
-        `https://site222326.tw.cs.unibo.it/squeals/?
-            startindex=${this.startIndex}&
-            endindex=${this.endIndex}
-            author=${this.query.author}&
-            popularity=${this.query.popularity}&
-            start_date=${this.query.startDate}&
-            end_date=${this.query.endDate}&
-            positive_reactions=${this.query.positiveReactions}&
-            negative_reactions=${this.query.negativeReactions}&
-            impressions=${this.query.impressions}&
-            receiver=${this.query.receiver}&
-            keyword=${this.query.keyword}&
-            mentions=${this.query.mentions}&
-            is_private=${this.query.isPrivate}
-            `
-      );
+      const response = await fetch(this.fetchURI);
       // assigns the json to the feed variable
       this.feed = await response.json();
         // lazy check to avoid making another request
@@ -311,23 +295,7 @@ export default {
           this.nextPageIsEmpty = true;
         } else {
           // check if next page is empty for sure
-          const response2 = await fetch(
-            `https://site222326.tw.cs.unibo.it/squeals/?startindex=${
-              this.startIndex + 11
-            }&endindex=${this.endIndex + 11}
-            author=${this.query.author}&
-            popularity=${this.query.popularity}&
-            start_date=${this.query.startDate}&
-            end_date=${this.query.endDate}&
-            positive_reactions=${this.query.positiveReactions}&
-            negative_reactions=${this.query.negativeReactions}&
-            impressions=${this.query.impressions}&
-            receiver=${this.query.receiver}&
-            keyword=${this.query.keyword}&
-            mentions=${this.query.mentions}&
-            is_private=${this.query.isPrivate}
-            `
-          );
+          const response2 = await fetch(this.fetchNextURI);
           const feed2 = await response2.json();
           console.log("feed2 length: " + feed2.length);
           if (feed2.length == 0) {
@@ -340,23 +308,7 @@ export default {
     async loadNext() {
       this.startIndex += 10;
       this.endIndex += 10;
-      const response = await fetch(
-        `https://site222326.tw.cs.unibo.it/squeals/?
-            startindex=${this.startIndex}&
-            endindex=${this.endIndex}
-            author=${this.query.author}&
-            popularity=${this.query.popularity}&
-            start_date=${this.query.startDate}&
-            end_date=${this.query.endDate}&
-            positive_reactions=${this.query.positiveReactions}&
-            negative_reactions=${this.query.negativeReactions}&
-            impressions=${this.query.impressions}&
-            receiver=${this.query.receiver}&
-            keyword=${this.query.keyword}&
-            mentions=${this.query.mentions}&
-            is_private=${this.query.isPrivate}
-            `
-      );
+      const response = await fetch(this.fetchURI);
       // assigns the json to the feed variable
       this.feed = await response.json();
       // check if feed is empty
@@ -372,23 +324,7 @@ export default {
         this.nextPageIsEmpty = true;
       } else {
         // check if next page is empty for sure
-        const response2 = await fetch(
-        `https://site222326.tw.cs.unibo.it/squeals/?
-            startindex=${this.startIndex + 11}&
-            endindex=${this.endIndex + 11}
-            author=${this.query.author}&
-            popularity=${this.query.popularity}&
-            start_date=${this.query.startDate}&
-            end_date=${this.query.endDate}&
-            positive_reactions=${this.query.positiveReactions}&
-            negative_reactions=${this.query.negativeReactions}&
-            impressions=${this.query.impressions}&
-            receiver=${this.query.receiver}&
-            keyword=${this.query.keyword}&
-            mentions=${this.query.mentions}&
-            is_private=${this.query.isPrivate}
-            `
-      );
+        const response2 = await fetch(this.fetchNextURI);
         const feed2 = await response2.json();
         if (feed2.length == 0) {
           this.nextPageIsEmpty = true;
@@ -404,23 +340,7 @@ export default {
 
       this.startIndex -= 10;
       this.endIndex -= 10;
-      const response = await fetch(
-        `https://site222326.tw.cs.unibo.it/squeals/?
-            startindex=${this.startIndex}&
-            endindex=${this.endIndex}
-            author=${this.query.author}&
-            popularity=${this.query.popularity}&
-            start_date=${this.query.startDate}&
-            end_date=${this.query.endDate}&
-            positive_reactions=${this.query.positiveReactions}&
-            negative_reactions=${this.query.negativeReactions}&
-            impressions=${this.query.impressions}&
-            receiver=${this.query.receiver}&
-            keyword=${this.query.keyword}&
-            mentions=${this.query.mentions}&
-            is_private=${this.query.isPrivate}
-            `
-      );
+      const response = await fetch(this.fetchURI);
       // assigns the json to the feed variable
       this.feed = await response.json();
       // lazy check to avoid making another request
@@ -428,23 +348,7 @@ export default {
         this.nextPageIsEmpty = true;
       } else {
         // check if next page is empty for sure
-        const response2 = await fetch(
-        `https://site222326.tw.cs.unibo.it/squeals/?
-            startindex=${this.startIndex + 11}&
-            endindex=${this.endIndex + 11}
-            author=${this.query.author}&
-            popularity=${this.query.popularity}&
-            start_date=${this.query.startDate}&
-            end_date=${this.query.endDate}&
-            positive_reactions=${this.query.positiveReactions}&
-            negative_reactions=${this.query.negativeReactions}&
-            impressions=${this.query.impressions}&
-            receiver=${this.query.receiver}&
-            keyword=${this.query.keyword}&
-            mentions=${this.query.mentions}&
-            is_private=${this.query.isPrivate}
-            `
-      );
+        const response2 = await fetch(this.fetchNextURI);
         const feed2 = await response2.json();
         if (feed2.length == 0) {
           this.nextPageIsEmpty = true;
