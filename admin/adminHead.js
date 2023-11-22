3fb0766b603da1bffc90ff1e76bd4e7cf4d8aa6b
mettil13 <matildemariano01@gmail.com>
2023-11-22 20:00:07 +0100

    playing with cheerio and vue

--- admin/adminHead.js
+++ admin/adminHead.js
@@ -1,2 +1,9 @@
-window.history.pushState({}, "", "https://site222326.tw.cs.unibo.it/app/channels");
+let currentUrl = window.location.href;
+
+const adminPrefix = "https://site222326.tw.cs.unibo.it/admin";
+if(currentUrl.startsWith(adminPrefix)) {
+    let relativeUrl = currentUrl.slice(adminPrefix.length);
+    window.history.pushState({}, "", `https://site222326.tw.cs.unibo.it/app/${relativeUrl}`);
+}
+
 console.log("head done!");
