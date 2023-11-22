641e04ac0e09a1c589a17f0f12839ece34192971
mettil13 <matildemariano01@gmail.com>
2023-11-22 20:41:02 +0100

    working admin links

--- admin/adminHead.js
+++ admin/adminHead.js
@@ -1,15 +1,32 @@
+const sitePrefix = "https://site222326.tw.cs.unibo.it";
+const adminPrefix = sitePrefix + "/admin";
+const appPrefix = sitePrefix + "/app";
+
+
 let currentUrl = window.location.href;
 
-const adminPrefix = "https://site222326.tw.cs.unibo.it/admin";
-if(currentUrl.startsWith(adminPrefix)) {
+if (currentUrl.startsWith(adminPrefix)) {
     let relativeUrl = currentUrl.slice(adminPrefix.length);
-    window.history.pushState({}, "", `https://site222326.tw.cs.unibo.it/app${relativeUrl}`);
+    window.history.pushState({}, "", appPrefix + relativeUrl);
 
-    window.onload = function() {
+    window.onload = function () {
         window.history.pushState({}, "", currentUrl);
+        replaceAllAppLinks();
     }
 }
 
 
 
 console.log("head done!");
+
+
+function replaceAllAppLinks() {
+    var anchors = document.getElementsByTagName("a");
+
+    for (var i = 0; i < anchors.length; i++) {
+        if (anchors[i].href.startsWith(appPrefix)){
+            let relativeUrl = currentUrl.slice(appPrefix.length);
+            anchors[i].href = adminPrefix + relativeUrl;
+        }
+    }
+}
