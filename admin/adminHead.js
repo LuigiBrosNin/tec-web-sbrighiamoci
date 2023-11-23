25c9d61f408b244b7937bf69128cf480cdde8a6c
mettil13 <matildemariano01@gmail.com>
2023-11-23 22:04:11 +0100

    code cleanup

--- admin/adminHead.js
+++ admin/adminHead.js
@@ -15,15 +15,10 @@ if (currentUrl.startsWith(adminPrefix)) {
 
 
     MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-    let observer = new MutationObserver(function (mutations, observer) {
-        mutations.forEach(mutation => {
-            if(mutation.target != "a"){
-                replaceAllAppLinks();
-                console.log("DOM changed", mutations, observer);
-                return;
-            }
-        })
-        
+    let observer = new MutationObserver(function (mutations, observer) {     // every time something changes in the DOM, this function is invoked
+        replaceAllAppLinks();
+
+        console.log("DOM changed", mutations, observer);
     });
     observer.observe(document, {
         subtree: true,
@@ -37,13 +32,22 @@ console.log("Admin loaded!");
 
 
 
-// functions
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 FUNCTIONS                                  */
+/*                     to make /admin different from /app                     */
+/* -------------------------------------------------------------------------- */
+// replaces all links that refer to /app with the same link that refers to /admin
+// e.g. https://site222326.tw.cs.unibo.it/app/foo -> https://site222326.tw.cs.unibo.it/admin/foo
 function replaceAllAppLinks() {
     const anchors = document.getElementsByTagName("a");
     const anchorsArray = Array.from(anchors);
 
     for(let i = 0; i < anchorsArray.length; i++){
         if (anchorsArray[i].href.startsWith(appPrefix)){
+            // we need to replace the entire <a> element (and not only the href) in order to make Vue ignore it
             anchorsArray[i].outerHTML = `<a href="${adminPrefix + anchorsArray[i].href.slice(appPrefix.length)}" class="${anchorsArray[i].getAttribute("class")}" id="${anchorsArray[i].id}"> ${anchorsArray[i].innerHTML} </a>`;
         }
     }
