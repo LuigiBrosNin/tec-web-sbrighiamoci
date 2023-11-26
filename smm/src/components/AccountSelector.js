41e93511fc500b8965939f7bd15244469115d61a
Luizo <emanuele109@hotmail.it>
2023-11-26 23:23:55 +0100

    smm account selector TODO profile n test

--- smm/src/components/AccountSelector.js
+++ smm/src/components/AccountSelector.js
@@ -1,37 +1,49 @@
 import { Axios } from 'axios'
 import React, { Component } from 'react'
+import ProfileCard from './ProfileCard';
 
 export class AccountSelector extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            accounts: []
+            accounts: [],
+            selectedAccount: ''
         };
+        this.handleAccountChange = this.handleAccountChange.bind(this);
     }
-  
+
+    handleAccountChange(event) {
+        Axios.get(`https://site222326.tw.cs.unibo.it/profiles/${event.target.value}`)
+            .then(res => {
+                const currentAccount = res.data;
+                this.setState({ selectedAccount: currentAccount });
+            })
+    }
+
+
     getAccounts() {
         Axios.get(`https://site222326.tw.cs.unibo.it/profiles/${this.$user}`)
-        .then(res => {
-            const accounts = res.data.smm_customers;
-            this.setState({ accounts });
-        })
+            .then(res => {
+                const accounts = res.data.smm_customers;
+                this.setState({ accounts });
+            })
     }
-  
+
     render() {
         this.getAccounts();
-    return (
-      <div>
+        return (
+            <div>
 
-        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>Account</span>
-        </h6>
-        <select className="form-select" aria-label="Account select">
-          <option selected>Choose an account</option>
-            {this.state.accounts.map(account => <option value={account}>{account}</option>)}
-        </select>
-      </div>
-    )
-  }
+                <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                    <ProfileCard profile={this.state.selectedAccount}/>
+                </h6>
+                <select className="form-select" aria-label="Default select example" value={this.state.selectedAccount} onChange={this.handleAccountChange}>
+                    <option value="">Choose an account</option>
+                    {this.state.accounts.map(account => <option key={account} value={account}>{account}</option>)}
+                </select>
+            </div>
+        )
+    }
 }
 
 export default AccountSelector
---
+++ smm/src/components/ProfileCard.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+
+import "profileCard.css"
+
+export class ProfileCard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            profile: props.profile
+        };
+    }
+
+    render() {
+        return (
+            <div class="row no-gutters">
+                <div class="col-md-4 d-flex justify-content-center align-items-center">
+                    <img src="profilePicUrl" class="card-img profile_img" alt="Profile Picture" />
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            { this.props.profile.name }
+                        </h5>
+                        <p class="card-text">{ this.props.profile.bio }</p>
+                        <div class="row credits_info">
+                            <div class="col">
+                                <p class="card-text">
+                                    Daily: { this.props.profile.credit[0] }/{ this.props.profile.creditLimits[0] }
+                                </p>
+                            </div>
+                            <div class="col">
+                                <p class="card-text">
+                                    Weekly: { this.props.profile.credit[1] }/{ this.props.profile.creditLimits[1] }
+                                </p>
+                            </div>
+                            <div class="col">
+                                <p class="card-text">
+                                    Monthly: { this.props.profile.credit[2] }/{ this.props.profile.creditLimits[2] }
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <p class="card-text">
+                                    Squeals: { this.props.profile.numberOfSqueals }
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ProfileCard
