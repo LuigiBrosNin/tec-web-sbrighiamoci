dd390accd10396edd2125e8144dc0dcc222fbfc3
alexlorenzato <alexlorenzato@gmail.com>
2024-01-04 16:53:09 +0100

    fix

--- app/src/views/ChannelCreateView.vue
+++ app/src/views/ChannelCreateView.vue
@@ -1,13 +1,6 @@
 <template>
 
-  <!-- Pulsanti di commutazione -->
-  <div class="d-flex justify-content-center my-3">
-    <button @click="showCreateSection" class="btn mx-2" :class="{ 'orange_btn': activeSection === 'create', 'btn-secondary': activeSection !== 'create' }">Create Channel</button>
-    <button @click="showModifySection" class="btn mx-2" :class="{ 'orange_btn': activeSection === 'modify', 'btn-secondary': activeSection !== 'modify' }">Modify Channel</button>
-  </div>
-
-  <!-------------------- CREAZIONE CANALE --------------------->
-  <div v-if="activeSection === 'create'" class="squeal_container">
+  <div class="squeal_container">
     <form @submit.prevent="createChannel" class="mt-5">
 
       <!-- Titolo -->
@@ -36,205 +29,56 @@
 
     </form>
   </div>
+
 </template>
 
 <script>
-import axios from "axios";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
 
-export default {
-  data() {
-    return {
-      activeSection: 'create',
-      new_channel_name: "",
-      tmp_channel_name: "",
-      new_bio: "",
-      channel_type: "",
-      bio: "",
-      mods: [],
-      mod_to_add: "",
-      max_bio_length: 150,
-      show_inputs: false,
-      search_channel_name: "",
-      file: null,
-      profilePicUrl: "",
-    };
-  },
-  methods: {
+  import axios from "axios";
+  import L from "leaflet";
+  import "leaflet/dist/leaflet.css";
 
-    async createChannel() {
-      try {
-        if (this.new_bio.length <= this.max_bio_length) {
-          const response = await axios.put(`https://site222326.tw.cs.unibo.it/channels/${this.new_channel_name}`, { bio: this.new_bio, type: this.channel_type } );
-          if (response.status === 409) {
-            alert("Channel name already taken, choose a different one.");
+  export default {
+    data() {
+      return {
+        new_channel_name: "",
+        tmp_channel_name: "",
+        new_bio: "",
+        channel_type: "",
+        max_bio_length: 150,
+        profilePicUrl: "",
+      };
+    },
+    methods: {
+
+      async createChannel() {
+        try {
+          if (this.new_bio.length <= this.max_bio_length) {
+            const response = await axios.put(`https://site222326.tw.cs.unibo.it/channels/${this.new_channel_name}`, { bio: this.new_bio, type: this.channel_type } );
+            if (response.status === 409) {
+              alert("Channel name already taken, choose a different one.");
+            }
+            else if (response.status === 401) {
+              alert("You're not authorized to create this channel.")
+            }
+            else if (response.status === 200) {
+              alert("Channel succesfully created!");
+            }
+            else {
+              alert("Something did not work, try later!");
+              console.error("Unexpected response status: ", response.status);
+            }
           }
-          else if (response.status === 401) {
-            alert("You're not authorized to create this channel.")
-          }
-          else if (response.status === 200) {
-            alert("Channel succesfully created!");
-          }
-          else {
-            alert("Something did not work, try later!");
-            console.error("Unexpected response status: ", response.status);
-          }
+          else { alert("Bio too long."); }
         }
-        else { alert("Bio too long."); }
-      }
-      catch (error) {
-        console.error("An error occured: ", error);
-      }
-    },
-
-    async searchChannel() {
-      try {
-        this.search_channel_name = this.tmp_channel_name
-        const response = await axios.get(`https://site222326.tw.cs.unibo.it/channels/${this.search_channel_name}`);
-        if (response.status === 200) {
-          this.show_inputs = true;
-          this.bio = response.data.bio;
-          this.channel_type = response.data.type;
-          this.mods = response.data.mod_list; 
-
-          this.profilePicUrl = response.data.propic
-
-          // <!--TODO  questa cosa della propic non funziona piu'
-          if (this.profilePicUrl == null || this.profilePicUrl == "") {
-            console.log("La propic è null.")
-            this.profilePicUrl =
-              "https://site222326.tw.cs.unibo.it/images/user-default.svg";
-          }
-          else {
-            console.log("La propic esiste.")
-            this.profilePicUrl = `https://site222326.tw.cs.unibo.it/channels/${this.search_channel_name}/propic`;
-          }   
+        catch (error) {
+          console.error("An error occured: ", error);
         }
-        else { alert("Canale non trovato."); }
-      }
-      catch (error) { console.error("An error occured: ", error); }
-    },
+      },
 
-    async updateBio() {
-      const response = await axios.post(`https://site222326.tw.cs.unibo.it/channels/${this.search_channel_name}`, { bio: this.bio });
-
-      if (response.status === 401) {
-        alert("You're not authorized to modify this channel.")
-      }
-      else if (response.status === 404) {
-        alert("Channel not found.")
-      }
-      else if (response.status === 200) {
-        alert("Channel updated succesfully!")
-      }
-    },
-
-    async addMod() {
-      const response = await axios.put(`https://site222326.tw.cs.unibo.it/channels/${this.search_channel_name}/mod_list`, { mod_name: this.mod_to_add } );
-
-      if (response.status === 400) {
-        alert("This mod is already on the list.")
-      }
-      else if (response.status === 404) {
-        alert("Channel or profile not found.")
-      }
-      else if (response.status === 401) {
-        alert("You're not authorized to modify the mods list.")
-      }
-      else if (response.status === 200) {
-        alert("Mod succesfully added!")
-
-        // dopo aver aggiunto un mod, ricarico la lista
-        const response = await axios.get(`https://site222326.tw.cs.unibo.it/channels/${this.search_channel_name}`);
-        this.mods = response.data.mod_list;
-        this.mod_to_add = "";
-      }
-    },
-
-    async removeMod(index) {
-      const mod_to_remove = this.mods.splice(index, 1)
-      console.log("rimuovo mod: ", mod_to_remove)
-      const response = await axios.delete(`https://site222326.tw.cs.unibo.it/channels/${this.search_channel_name}/mod_list`, { data: { mod_name: mod_to_remove }});
-
-      if (response.status === 404) {
-        alert("Channel or profile not found.")
-      }
-      else if (response.status === 401) {
-        alert("You're not authorized to modify the mods list.")
-      }
-      else if (response.status === 200) {
-        alert("Mod succesfully removed!")
-
-        // dopo aver rimosso un mod, ricarico la lista
-        const response = await axios.get(`https://site222326.tw.cs.unibo.it/channels/${this.search_channel_name}`);
-        this.mods = response.data.mod_list;
-        this.mod_to_add = "";
-      }
-    },
-
-    showCreateSection() {
-      this.activeSection = 'create';
-    },
-    showModifySection() {
-      this.activeSection = 'modify';
-    },
-
-    handleFileUpload(event) {
-      this.file = event.target.files[0];
-    },
-
-    uploadFile() {
-      const formData = new FormData();
-      formData.append("file", this.file);
-
-      console.log("FORMDATA: ", formData)
-      console.log("CHANNELNAME: ", this.search_channel_name)
-
-      axios
-        .put(
-          `https://site222326.tw.cs.unibo.it/channels/${this.search_channel_name}/propic`,
-          formData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-            onUploadProgress: (progressEvent) => {
-              console.log(
-                "Upload progress:" +
-                Math.round(
-                  (progressEvent.loaded / progressEvent.total) * 100
-                ) +
-                "%"
-              );
-            },
-          }
-        )
-        .then((response) => {
-          console.log(response.data);
-          // notify the user that the upload was successful
-          alert("Upload successful");
-          this.profilePicUrl = `https://site222326.tw.cs.unibo.it/channels/${this.search_channel_name}/propic`;
-        });
-    }, 
-
-    removePic() {
-      const name_of_profile = this.$user;
-      axios
-        .delete(
-          `https://site222326.tw.cs.unibo.it/channels/${this.search_channel_name}/propic`
-        )
-        .then((response) => {
-          console.log(response.data);
-          // notify the user that the upload was successful
-          alert("Profile picture removed");
-          profilePicUrl =
-            "https://site222326.tw.cs.unibo.it/images/user-default.svg";
-        });
-    },
-  
-  }
+    }
 }
+  
 </script>
 
 
@@ -273,5 +117,5 @@ export default {
     height: 10em;
     border-radius: 50%;
     margin: 0.5em;
-}
+  }
 </style>
