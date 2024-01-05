3d0679b619f9df28aba5d8be0c19ee74a67018f7
mettil13 <matildemariano01@gmail.com>
2024-01-05 21:16:31 +0100

    better adminEditSqueal

--- admin/adminEditSqueal.js
+++ admin/adminEditSqueal.js
@@ -9,7 +9,7 @@ if (window.location.href.startsWith(editSquealPrefix)) {
     console.log(squeal_id);
 
 
-    
+
     squeal = await fetch(`https://site222326.tw.cs.unibo.it/squeals/${squeal_id}`, {
         method: "GET"
     });
@@ -21,7 +21,7 @@ if (window.location.href.startsWith(editSquealPrefix)) {
 
 
 document.getElementById('squeal_form').addEventListener('submit', async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target).entries());
     console.log(data);
 
@@ -29,62 +29,58 @@ document.getElementById('squeal_form').addEventListener('submit', async (e) => {
     let changes = {};
 
     // id and author cannot be modified
-    if(data.receiver != null && data.receiver != "" && data.receiver != squeal.receiver){
+    if (data.receiver != null && data.receiver != "" && data.receiver != squeal.receiver) {
         changes.receiver = data.receiver;
     }
 
-    if(data.isPrivate != null && data.isPrivate != "" && data.isPrivate != squeal.is_private){
+    if (data.isPrivate != null && data.isPrivate != "" && data.isPrivate != squeal.is_private) {
         changes.is_private = data.isPrivate;
     }
 
-    if(data.replyTo != null && data.replyTo != "" && squeal.reply_to != true){
+    if (data.replyTo != null && data.replyTo != "" && squeal.reply_to != true) {
         changes.reply_to = true;
     }
-    else if((data.replyTo == null || data.replyTo == "") && squeal.reply_to != false){
+    else if ((data.replyTo == null || data.replyTo == "") && squeal.reply_to != false) {
         changes.reply_to = false;
     }
 
-    if(data.squealText != null && data.squealText != "" && data.squealText != squeal.text){
+    if (data.squealText != null && data.squealText != "" && data.squealText != squeal.text) {
         changes.text = data.squealText;
     }
 
-    if(data.squealDate != null && data.squealDate != "" && data.squealDate != squeal.date){
+    if (data.squealDate != null && data.squealDate != "" && data.squealDate != squeal.date) {
         changes.date = data.squealDate;
     }
 
-    if(data.positiveReactions != null && data.positiveReactions != "" && data.positiveReactions != squeal.positive_reactions){
+    if (data.positiveReactions != null && data.positiveReactions != "" && data.positiveReactions != squeal.positive_reactions) {
         changes.positive_reactions = data.positiveReactions;
     }
 
-    if(data.negativeReactions != null && data.negativeReactions != "" && data.negativeReactions != squeal.negative_reactions){
+    if (data.negativeReactions != null && data.negativeReactions != "" && data.negativeReactions != squeal.negative_reactions) {
         changes.negative_reactions = data.negativeReactions;
     }
 
-    if(data.replies != null && data.replies != "" && data.replies != squeal.replies_num){
+    if (data.replies != null && data.replies != "" && data.replies != squeal.replies_num) {
         changes.replies_num = data.replies;
     }
 
-    if(data.impressions != null && data.impressions != "" && data.impressions != squeal.impressions){
+    if (data.impressions != null && data.impressions != "" && data.impressions != squeal.impressions) {
         changes.impressions = data.impressions;
     }
 
+    if (data.deleteMedia != null && data.deleteMedia != "") {
+        changes.media = "";
+    }
 
-
-
-    // MEDIA
-    
-
-
-
-    if(data.locationLat != null && data.locationLat != "" && data.locationLat != squeal.location.latitude){
-        if(changes.location == null){
+    if (data.locationLat != null && data.locationLat != "" && data.locationLat != squeal.location.latitude) {
+        if (changes.location == null) {
             changes.location = {};
         }
         changes.location.latitude = data.locationLat;
     }
 
-    if(data.locationLng != null && data.locationLng != "" && data.locationLng != squeal.location.longitude){
-        if(changes.location == null){
+    if (data.locationLng != null && data.locationLng != "" && data.locationLng != squeal.location.longitude) {
+        if (changes.location == null) {
             changes.location = {};
         }
         changes.location.longitude = data.locationLng;
@@ -95,12 +91,11 @@ document.getElementById('squeal_form').addEventListener('submit', async (e) => {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
-          },
+        },
         body: JSON.stringify(changes)
     });
     if (res.status == 201) {
-        console.log("Updated!");
-        //window.location.href = `${sitePrefix}/admin/squeal/${squeal.id}`;
+        window.location.href = `${sitePrefix}/admin/squeal/${squeal.id}`;
     } else {
         alert("an error has occurred, please try again later");
     }
@@ -113,7 +108,7 @@ document.getElementById('squeal_form').addEventListener('submit', async (e) => {
 
 
 
-function populate(squeal_json){
+function populate(squeal_json) {
     document.getElementById("squeal_id").setAttribute("value", squeal_json.id);
     document.getElementById("author").setAttribute("value", squeal_json.author);
     document.getElementById("receiver").setAttribute("value", squeal_json.receiver);
@@ -125,15 +120,15 @@ function populate(squeal_json){
     document.getElementById("negativeReactions").setAttribute("value", squeal_json.negative_reactions);
     document.getElementById("replies").setAttribute("value", squeal_json.replies_num);
     document.getElementById("impressions").setAttribute("value", squeal_json.impressions);
-    
-    if(squeal_json.media != null && squeal_json.media != ""){
+
+    if (squeal_json.media != null && squeal_json.media != "") {
         document.getElementById("oldMedia").innerHTML = `<img src="https://${squeal_json.media}">`;
     }
-    
-    if(squeal_json.location.latitude != null){
+
+    if (squeal_json.location.latitude != null) {
         document.getElementById("locationLat").setAttribute("value", squeal_json.location.latitude);
     }
-    if(squeal_json.location.longitude != null){
+    if (squeal_json.location.longitude != null) {
         document.getElementById("locationLng").setAttribute("value", squeal_json.location.longitude);
     }
 }
