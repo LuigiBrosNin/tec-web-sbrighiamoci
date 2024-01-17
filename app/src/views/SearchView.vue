3c78d393f6d382755ebd3edd4d0e3d7a894de682
mettil13 <matildemariano01@gmail.com>
2024-01-17 11:00:09 +0100

    changed searchView tabs id

--- app/src/views/SearchView.vue
+++ app/src/views/SearchView.vue
@@ -7,33 +7,33 @@
 <template>
     <ul class="nav nav-pills mb-3 flex-column flex-sm-row" id="pills-tab" role="tablist">
         <li class="nav-item flex-sm-fill text-sm-center" role="presentation">
-            <button class="nav-link active" id="pills-channels-tab" data-bs-toggle="pill" data-bs-target="#pills-channels"
-                type="button" role="tab" aria-controls="pills-channels" aria-selected="true">
+            <button class="nav-link active" id="pills-channels-tab" data-bs-toggle="pill" data-bs-target="#search-channels"
+                type="button" role="tab" aria-controls="search-channels" aria-selected="true">
                 Channels
             </button>
         </li>
         <li class="nav-item flex-sm-fill text-sm-center" role="presentation">
-            <button class="nav-link" id="pills-profiles-tab" data-bs-toggle="pill" data-bs-target="#pills-profiles"
-                type="button" role="tab" aria-controls="pills-profiles" aria-selected="false">
+            <button class="nav-link" id="pills-profiles-tab" data-bs-toggle="pill" data-bs-target="#search-profiles"
+                type="button" role="tab" aria-controls="search-profiles" aria-selected="false">
                 Profiles
             </button>
         </li>
         <li class="nav-item flex-sm-fill text-sm-center" role="presentation">
-            <button class="nav-link" id="pills-squeals-tab" data-bs-toggle="pill" data-bs-target="#pills-squeals"
-                type="button" role="tab" aria-controls="pills-squeals" aria-selected="false">
+            <button class="nav-link" id="pills-squeals-tab" data-bs-toggle="pill" data-bs-target="#search-squeals"
+                type="button" role="tab" aria-controls="search-squeals" aria-selected="false">
                 Squeals
             </button>
         </li>
     </ul>
 
     <div class="tab-content" id="pills-tabContent">
-        <div class="tab-pane fade show active" id="pills-channels" role="tabpanel" aria-labelledby="pills-channels-tab">
+        <div class="tab-pane fade show active" id="search-channels" role="tabpanel" aria-labelledby="pills-channels-tab">
             <SearchChannelsView></SearchChannelsView>
         </div>
-        <div class="tab-pane fade" id="pills-profiles" role="tabpanel" aria-labelledby="pills-profiles-tab">
+        <div class="tab-pane fade" id="search-profiles" role="tabpanel" aria-labelledby="pills-profiles-tab">
             <SearchProfilesView></SearchProfilesView>
         </div>
-        <div class="tab-pane fade" id="pills-squeals" role="tabpanel" aria-labelledby="pills-squeals-tab">
+        <div class="tab-pane fade" id="search-squeals" role="tabpanel" aria-labelledby="pills-squeals-tab">
             <SearchSquealsView></SearchSquealsView>
         </div>
     </div>
